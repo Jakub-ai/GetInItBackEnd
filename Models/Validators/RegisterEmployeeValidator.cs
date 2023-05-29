10a45b6d408e37a696f11dfc6945ece4d38aa88d
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2023-05-29 19:06:16 +0200

    Creating Employee validator added

---
+++ Models/Validators/RegisterEmployeeValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using GetInItBackEnd.Entities;
+using GetInItBackEnd.Models.Account;
+
+namespace GetInItBackEnd.Models.Validators;
+
+public class RegisterEmployeeValidator : AbstractValidator<CreateEmployeeDto>
+{
+    public RegisterEmployeeValidator(GetInItDbContext dbContext)
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(x => x.Email)
+            .Custom((value, context) =>
+            {
+                var emailInUse = dbContext.Accounts.Any(u => u.Email == value);
+                if (emailInUse)
+                {
+                    context.AddFailure("Email", "Email is taken!");
+                }
+            });
+        RuleFor(x => x.Password)
+            .MinimumLength(6);
+        RuleFor(x => x.ConfirmPassword).Equal(e => e.Password);
+    }
+}
